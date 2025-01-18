375fca0edcb49cb26a025d2bb6e18d5ed64f030b
crashAndBurns <frankeciszek@gmail.com>
2025-01-17 23:43:39 -0500

    Commit of Module 1 exercises

---
+++ Module_1_Exercise/Module1.js
@@ -0,0 +1,25 @@
+// External JavaScript file for use with Module1.html
+// This file contains the functions used by the buttons in the 
+// HTML file Module1.html authored by Frank E. Ciszek on 1/17/2025
+// For the Class  CIT-215 at West Virginia Northern Community College
+
+
+function answer2() {
+    document.getElementById("answer2").innerHTML=" \
+    JavaScript is a web programming language. It can update and change \
+    HTML and CSS attributes. It can also calculate, manipulate, and \
+    validate data. While HTML is used to define content of a web page \
+    and CSS is used to specify the layout of the page, JavaScript is \
+    used to program the behavior of the page.";
+}
+
+function answer3() {
+    document.getElementById("answer3").innerHTML=" \
+        Major libraries include jQuery, Angular, Bootsrap, and many \
+        others. About 80% of all websites use third party libraries but \
+        some 'Vanilla' websites rely soley on standard JavaScript. Other \
+        projects include both client-side and server side projects such as \
+        Electorn, Cordova, and React Native as well as Adobe Acrobat PDF \
+        scripting and GNOME Shell extensions. Much of this was made possible \
+        by using Node.js to run JavaScript outside the browser environment.";
+}
